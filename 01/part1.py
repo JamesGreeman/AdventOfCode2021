1b0bdcf1c1bd397186e9b04245554252addfb785
James Greeman <james.greeman@geospock.com>
2021-12-03 10:44:29 +0000

    Committing missing changes for day 1 part 1

--- 01/part1.py
+++ 01/part1.py
@@ -1,18 +1,11 @@
 lines = open('input.txt', 'r').readlines()
 
-queue = []
+previous_depth = None
 increases = 0
 for line in lines:
     line = line.strip()
-    if not line == '':
-        current_depth = int(line)
-        if len(queue) == 3:
-            current_size = sum(queue)
-            queue.pop(0)
-            queue.append(current_depth)
-            new_size = sum(queue)
-            if new_size > current_size:
-                increases += 1
-        else:
-            queue.append(int(line))
+    current_depth = int(line)
+    if previous_depth and current_depth > previous_depth:
+        increases += 1
+    previous_depth = current_depth
 print(increases)
