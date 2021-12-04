426cee4d8b7628f1117a256485d1d68b9a955560
James Greeman <james.greeman@geospock.com>
2021-12-04 14:35:23 +0000

    Added comments for part 2

--- 04/part2.py
+++ 04/part2.py
@@ -1,3 +1,8 @@
+# Reads the source file
+#  - first line is read as a list of integers, this is the sequence of numbers calls
+#  - subsequent lines are chunked into groups of 5 for the bingo boards
+#    They are parsed as integer values, paired with a boolean (this will indicate if they are marked or not during
+#    the game.
 def read_data():
     lines = [line.strip() for line in open('input.txt', 'r').readlines()]
 
@@ -10,11 +15,12 @@ def read_data():
         for i, line in enumerate(chunk):
             board.append([])
             for j, value in enumerate([value for value in line.strip().split(" ") if not value == '']):
-                board[i].append((int(value), False))  # False as all values are not identified at the start
+                board[i].append((int(value), False))  # False as all values are not marked at the start
         boards.append(board)
     return sequence, boards
 
 
+# Looks at all values on a board, if the specified value is found then it is marked as True
 def update_board(board, value):
     for i, line in enumerate(board):
         for j, (board_value, _) in enumerate(line):
@@ -22,11 +28,13 @@ def update_board(board, value):
                 board[i][j] = value, True
 
 
+# Checks for horizontal, vertical or diagonal winning lines, return True if any are found
 def is_winning_board(board):
-    return has_winning_horizonal(board) or has_winning_vertical(board) or has_winning_diagonal(board)
+    return has_winning_horizontal(board) or has_winning_vertical(board) or has_winning_diagonal(board)
 
 
-def has_winning_horizonal(board):
+# Checks for any horizontal line where all the values are marked as True
+def has_winning_horizontal(board):
     for line in board:
         for i, (_, marked) in enumerate(line):
             if not marked:
@@ -37,6 +45,7 @@ def has_winning_horizonal(board):
     return False
 
 
+# Checks for any vertical line where all the values are marked as True
 def has_winning_vertical(board):
     for column in range(5):
         for row in range(5):
@@ -50,6 +59,7 @@ def has_winning_vertical(board):
     return False
 
 
+# Checks if either diagonal has all the values are marked as True
 def has_winning_diagonal(board):
     for i in range(5):
         _, marked = board[i][i]
@@ -68,11 +78,13 @@ def has_winning_diagonal(board):
     return False
 
 
+# Flattens the liosts and filters marked values, then sums all the values
 def get_total_unmarked_values(board):
     unmarked_values = [value for sublist in board for value, marked in sublist if not marked]
     return sum(unmarked_values)
 
 
+# Pretty prints the current state of the board
 def print_board_status(comment, board):
     print(comment)
     [print(f'{["X" if marked else "O" for _, marked in line]} -- {line}') for line in board]
@@ -88,6 +100,8 @@ def main():
             update_board(board, value)
             if not is_winning_board(board):
                 remaining_boards.append(board)
+
+        # If after playing all boards we have none left we know the last board of the list was the last to win.
         if len(remaining_boards) == 0:
             final_board = boards[len(boards) - 1]
             unmarked_total = get_total_unmarked_values(final_board)
