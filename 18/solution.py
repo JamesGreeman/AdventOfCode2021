373ece1a9beadedfc038475df0e1a7432fb3de20
Zanvork <jamie.greeman@gmail.com>
2022-01-05 15:35:32 +0000

    Cleaned up code to use a Polymorphic solution

--- 18/solution.py
+++ 18/solution.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
 import json
-import re
-from typing import Tuple, Dict, List, Optional
-
-snail_number_regex = re.compile("\\[(\\d|\\[.*\\]),(\\d|\\[.*\\])\\]")
+from abc import ABC, abstractmethod
+from typing import Tuple, List, Optional
 
 
 def read_raw_input() -> List[List[str]]:
@@ -12,7 +10,7 @@ def read_raw_input() -> List[List[str]]:
     lists = []
     numbers = []
 
-    for line in open('input.txt', 'r').readlines():
+    for line in open('sample.txt', 'r').readlines():
         line = line.strip()
         if not line:
             lists.append(numbers)
@@ -25,145 +23,153 @@ def read_raw_input() -> List[List[str]]:
     return lists
 
 
-def read_input() -> List[List[SnailNumber]]:
-    lists = []
-    numbers = []
-
-    for line in open('input.txt', 'r').readlines():
-        line = line.strip()
-        if not line:
-            lists.append(numbers)
-            numbers = []
-        else:
-            numbers.append(parse_input(json.loads(line)))
-
-    if numbers:
-        lists.append(numbers)
-    return lists
+def read_input() -> List[List[Node]]:
+    return [[parse_input(json.loads(line)) for line in lines] for lines in read_raw_input()]
 
 
-def parse_input(input):
-    if isinstance(input, list):
-        left = parse_input(input[0])
-        right = parse_input(input[1])
+def parse_input(input_str) -> Node:
+    if isinstance(input_str, list):
+        left = parse_input(input_str[0])
+        right = parse_input(input_str[1])
 
-        return SnailNumber(left, right)
-    return int(input)
+        return PairNode(left, right)
+    return ValueNode(int(input_str))
 
 
-class SnailNumber:
+class Node(ABC):
 
-    def __init__(self, left, right):
+    @abstractmethod
+    def add(self, node: Node):
+        pass
+
+    @abstractmethod
+    def get_magnitude(self) -> int:
+        pass
+
+    @abstractmethod
+    def get_split(self) -> Optional[PairNode] | bool:
+        pass
+
+    @abstractmethod
+    def get_exploded(self, depth: int) -> Optional[Tuple[Optional[int], Optional[int]]]:
+        pass
+
+    @abstractmethod
+    def add_exploded_value(self, value: int, left: bool) -> bool:
+        pass
+
+
+class ValueNode(Node):
+
+    def __init__(self, value: int):
+        self.value = value
+
+    def get_magnitude(self) -> int:
+        return self.value
+
+    def add(self, node: Node):
+        raise NotImplementedError("Cannot add a node to a value node")
+
+    def get_split(self) -> Optional[PairNode] | bool:
+        if self.value >= 10:
+            left_value = self.value // 2
+            right_value = (self.value + 1) // 2
+            return PairNode(ValueNode(left_value), ValueNode(right_value))
+        return None
+
+    def get_exploded(self, depth: int) -> Optional[Tuple[Optional[int], Optional[int]]]:
+        return None
+
+    def add_exploded_value(self, value: int, left: bool) -> bool:
+        self.value += value
+        return True
+
+    def __repr__(self) -> str:
+        return str(self.value)
+
+
+class PairNode(Node):
+
+    def __init__(self, left: Node, right: Node):
         self.left = left
         self.right = right
 
-    def add(self, another: SnailNumber):
-        self.left = SnailNumber(self.left, self.right)
+    def add(self, another: Node):
+        self.left = PairNode(self.left, self.right)
         self.right = another
         self._normalise()
 
     def _normalise(self):
-        if self.try_explode():
+        if self.get_exploded():
             self._normalise()
             return
-        if self.try_split():
+        if self.get_split():
             self._normalise()
             return
 
     def get_magnitude(self):
-        left_mag = self.left if isinstance(self.left, int) else self.left.get_magnitude()
-        right_mag = self.right if isinstance(self.right, int) else self.right.get_magnitude()
+        return 3 * self.left.get_magnitude() + 2 * self.right.get_magnitude()
 
-        return 3 * left_mag + 2 * right_mag
+    def add_exploded_value(self, value: int, left: bool) -> bool:
+        node_to_add = self.left if left else self.right
+        if node_to_add.add_exploded_value(value, left):
+            return True
+        return False
 
-    def add_exploded_value(self, value: int, left: bool):
-        next_node = self.left if left else self.right
-
-        if isinstance(next_node, int):
-            if left:
-                self.left += value
-            else:
-                self.right += value
-        else:
-            next_node.add_exploded_value(value, left)
-
-    def try_explode(self, depth: int = 0) -> Optional[Tuple[Optional[int], Optional[int]]]:
-        # If any pair is nested inside four pairs, the leftmost such pair explodes.
-
+    def get_exploded(self, depth: int = 0) -> Optional[Tuple[Optional[int], Optional[int]]]:
         if depth == 4:
-            return self.left, self.right
+            # Nodes at depth 4 are always two values - so we can use the magnitude
+            return self.left.get_magnitude(), self.right.get_magnitude()
 
-        if isinstance(self.left, SnailNumber):
-            left_result = self.left.try_explode(depth + 1)
-            if left_result:
-                if left_result[0] is not None and left_result[1] is not None:
-                    self.left = 0
-                if left_result[1] is not None:
-                    if isinstance(self.right, int):
-                        self.right += left_result[1]
-                    else:
-                        self.right.add_exploded_value(left_result[1], True)
-                return left_result[0], None
+        left_exploded = self.left.get_exploded(depth + 1)
+        if left_exploded:
+            if left_exploded[1] is not None:
+                if left_exploded[0] is not None:
+                    self.left = ValueNode(0)
+                self.right.add_exploded_value(left_exploded[1], True)
+            return left_exploded[0], None
 
-        if isinstance(self.right, SnailNumber):
-            right_result = self.right.try_explode(depth + 1)
-            if right_result:
-                if right_result[0] is not None and right_result[1] is not None:
-                    self.right = 0
-                if right_result[0] is not None:
-                    if isinstance(self.left, int):
-                        self.left += right_result[0]
-                    else:
-                        self.left.add_exploded_value(right_result[0], False)
-                return None, right_result[1]
+        right_exploded = self.right.get_exploded(depth + 1)
+        if right_exploded:
+            if right_exploded[0] is not None:
+                if right_exploded[1] is not None:
+                    self.right = ValueNode(0)
+                self.left.add_exploded_value(right_exploded[0], False)
+            return None, right_exploded[1]
         return None
 
-    def try_split(self) -> bool:
-        # If any regular number is 10 or greater, the leftmost such regular number splits.
-
-        if isinstance(self.left, int):
-            if self.left >= 10:
-                self.left = SnailNumber(self.left // 2, (self.left + 1) // 2)
-                return True
-        else:
-            split = self.left.try_split()
-            if split:
-                return True
-
-        if isinstance(self.right, int):
-            if self.right >= 10:
-                self.right = SnailNumber(self.right // 2, (self.right + 1) // 2)
-                return True
-        else:
-            split = self.right.try_split()
-            if split:
-                return True
-
-        # To split a regular number, replace it with a pair; the left element of the pair should be the regular number
-        # divided by two and rounded down, while the right element of the pair should be the regular number divided by
-        # two and rounded up. For example, 10 becomes [5,5], 11 becomes [5,6], 12 becomes [6,6], and so on.
-        return False
+    def get_split(self) -> Optional[Node] | bool:
+        left_split = self.left.get_split()
+        if left_split:
+            if isinstance(left_split, Node):
+                self.left = left_split
+            return True
+        right_split = self.right.get_split()
+        if right_split:
+            if isinstance(right_split, Node):
+                self.right = right_split
+            return True
+        return None
 
     def __repr__(self):
         return f"[{self.left},{self.right}]"
 
+
 def find_largest_score():
     raw_numbers = read_raw_input()
 
-    for list in raw_numbers:
+    for num_list in raw_numbers:
         max_score = 0
-        for num_a in list:
-            for num_b in list:
+        for num_a in num_list:
+            for num_b in num_list:
                 if num_a != num_b:
                     a = parse_input(json.loads(num_a))
                     a.add(parse_input(json.loads(num_b)))
                     mag = a.get_magnitude()
                     max_score = max(max_score, mag)
-        print(list)
+        print(num_list)
         print(max_score)
         print()
-
-
 
 
 def main():
